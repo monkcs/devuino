87ce4106eed12ff0b6bc4313b341a2f383759133
Monkcs studios <ape@monkcs.se>
2018-04-25 21:56:07 +0200

    Change division to multiplication to get right SI-prefix value

--- source/Distance/Distance.cpp
+++ source/Distance/Distance.cpp
@@ -1,19 +1,19 @@
 #include "Distance.h"
 
 float Distance::decimetre() {
-    return metre() / 10.0;
+    return metre() * 10.0;
 }
 
 float Distance::centimetre() {
-    return metre() / 100.0;
+    return metre() * 100.0;
 }
 
 float Distance::millimetre() {
-    return metre() / 1000.0;
+    return metre() * 1000.0;
 }
 
 float Distance::micrometre() {
-    return metre() / 1000000.0;
+    return metre() * 1000000.0;
 }
 
 float Distance::yard() {
