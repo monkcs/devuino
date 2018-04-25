473b98b5c7080415bc88a3a41d9cf3a48721eb9b
Monkcs studios <ape@monkcs.se>
2018-04-25 17:51:44 +0200

    Add '.0' to flag floating point division

--- source/Light/Light.cpp
+++ source/Light/Light.cpp
@@ -4,7 +4,7 @@ int Light::brightness() {
     return bright;
 }
 void Light::brightness(float percent) {
-    // brightness(255 * percent);
-    // brightness(1023 * percent);
-    // brightness(4095 * percent);
+    // brightness(255.0 * percent);
+    // brightness(1023.0 * percent);
+    // brightness(4095.0 * percent);
 }
