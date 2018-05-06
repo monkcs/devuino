373449badcb9251bd02afdbc5e2423397546781c
Monkcs studios <ape@monkcs.se>
2018-05-06 17:07:23 +0200

    Add example class

---
+++ source/template/Example.cpp
@@ -0,0 +1,8 @@
+#include "Example.h"
+namespace devuino
+{
+    float Example::dsadsabgngf()
+    {
+        return 3.14;
+    }
+}
---
+++ source/template/Example.h
@@ -0,0 +1,15 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+namespace devuino
+{
+    class Example
+    {
+      public:
+        virtual float dsahdsabhdasj() = 0;
+        float dsadsabgngf();
+        float hnnjjybfhg();
+    };
+}
+
+#endif
