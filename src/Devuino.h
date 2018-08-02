de2a61a49049c4a9d447877a06826d7b59af793d
Charlie Habolin <charlie@localhost.localdomain>
2018-08-02 14:26:48 +0200

    Create a "import all" file

---
+++ src/Devuino.h
@@ -0,0 +1,35 @@
+
+
+/* 
+   
+   -- Devuino -----------------------------------------------------------
+   Devuino (https://github.com/monkcs/devuino) created by Charlie habolin
+   
+   The Devuino standard library aims to unify the library implementations
+   for different devices to make clearer code and promote code reuse.
+
+   If you want to write or add a device to the library, or just give a
+   suggestion for a specific device, open a issue on the git project page.
+
+
+   -- License -----------------------------------------------------------
+   All code, files or other assets in this project is licensed under the
+   GNU Lesser General Public License v3.0.
+   ----------------------------------------------------------------------
+
+   -- This file ---------------------------------------------------------
+   To make it easier to use this library by the everyday user, this file
+   contains #import's and namespace import to put all the different parts
+   into scope.
+   ----------------------------------------------------------------------
+
+
+*/
+
+
+
+
+/* Put all namespaces into scope */
+using namespace devuino;
+using namespace devuino::pin;
+using namespace devuino::device;
