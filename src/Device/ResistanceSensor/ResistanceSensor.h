8f804ae0a16a6d199e187238be295f64d64cc4ff
Monkcs studios <ape@monkcs.se>
2019-03-01 01:02:46 +0100

    Add ResistanceSensor device

---
+++ src/Device/ResistanceSensor/ResistanceSensor.h
@@ -0,0 +1,58 @@
+#ifndef RESISTANCESENSOR_H
+#define RESISTANCESENSOR_H
+
+#include "../../Input/InputAnalog.h"
+#include "../../Delay/DelaySync.h"
+
+namespace devuino
+{
+    namespace device
+    {
+        template <typename T>
+        class ResistanceSensor : public InputAnalog
+        {
+          public:
+
+            /* Make a reading of a analog input from (T signal) by powering on a resistor from the (T power) pin.
+             * That to prevent for example powerdraw and corrosion on water sensors. */
+
+            ResistanceSensor(T signal, T power, bool debounce = false, uint8_t iterations = 10, uint8_t bitresolution = 8)
+                : signal(signal), power(power), iterations(iterations), InputAnalog(bitresolution, debounce)
+            {
+                this->signal.initiate(pin::Mode::InputAnalog);
+                this->power.initiate(pin::Mode::OutputDigital);
+            }
+
+            int value() override
+            {
+                power.digitalwrite(true);
+                int value = 0;
+
+                if (debounce)
+                {
+                    int reading = 0;
+                    for (uint8_t counter = 0; counter < iterations; counter++)
+                    {
+                        reading += signal.analogread();
+                        DelaySync(5);
+                    }
+                    value = reading / iterations;
+                }
+                else
+                {
+                    value = signal.analogread();
+                }
+
+                power.digitalwrite(false);
+                return value;
+            }
+
+          protected:
+            T signal;
+            T power;
+            const uint8_t iterations;
+        };
+    }
+}
+
+#endif
