8fde768c202836c102599ea93399252b015ee395
Monkcs studios <ape@monkcs.se>
2019-03-03 22:51:06 +0100

    Move function into header

--- src/Input/InputAnalog.h
+++ src/Input/InputAnalog.h
@@ -8,9 +8,14 @@ namespace devuino
     class InputAnalog
     {
       public:
-        InputAnalog(int bitresolution, bool debounce);
+        InputAnalog(int bitresolution, bool debounce)
+        : bitsize(bitresolution), debounce(debounce) {};
 
-        double percent();
+        double percent()
+        {
+            return (double)value() / (double)(bitsize.resolution - 1);
+        };
+
         virtual int value() = 0;
 
       protected:
