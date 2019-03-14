4406e7107782a4b24f79fe1313888a0bf77d9ee1
Monkcs studios <ape@monkcs.se>
2019-03-14 16:22:48 +0100

    Move code into header

--- src/Delay/DelayAsync.h
+++ src/Delay/DelayAsync.h
@@ -10,10 +10,43 @@ namespace devuino
            expired.
            */
         public:
-            DelayAsync(const uint32_t milliseconds);
+            DelayAsync(const uint32_t milliseconds) : delaytime(milliseconds)
+            {
+                /*
+                Invoke update function to assign a target value.
+                */
 
-            void delay(const uint32_t milliseconds);
-            bool reached();
+                reached();
+            }
+
+            void delay(const uint32_t milliseconds)
+            {
+                /*
+                Before assigning a new delay, remove the old
+                one from "target" to preserve the last time
+                assigned from reached(). Then assign the new
+                delay.
+                */
+
+                target -= delaytime;
+                delaytime = milliseconds;
+                target += delaytime;
+            }
+
+            bool reached()
+            {
+                const auto time = millis();
+
+                if (time <= target)
+                {
+                    return false;
+                }
+                else
+                {
+                    target = time + delaytime;
+                    return true;
+                }
+            }
 
         private:
             uint32_t target;
