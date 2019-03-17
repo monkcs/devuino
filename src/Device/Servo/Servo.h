b99349f94bd1c108632f613479b73d736e019750
Monkcs studios <ape@monkcs.se>
2019-03-17 15:12:26 +0100

    Remove include to old .cpp file

--- src/Device/Servo/Servo.h
+++ src/Device/Servo/Servo.h
@@ -3,66 +3,74 @@
 
 #include "../../Output/OutputDigital.h"
 #include "../../Rotation/Rotation.h"
+#include "../../Delay/DelayAsync.h"
 
 namespace devuino
 {
     namespace device
     {
+        // Just for test
+
+        template <class T>
+            constexpr T map(const T value, const T inputmin, const T inputmax, const T outputmin, const T outputmax)
+            {
+                return (value - inputmin) * (outputmax - outputmin) / (inputmax - inputmin) + outputmin;
+            };
+
+
         template <typename T>
-        class Servo : public OutputDigital, Rotation
+            class Servo : public OutputDigital, Rotation
         {
-          public:
-            Servo(T pin)
-                : pin(pin)
-            {
-                this->pin.initiate(pin::Mode::OutputDigital);
-                off();
-            }
-            ~Servo()
-            {
-                off();
-            }
-
-            void change(bool state)
-            {
-                active = state;
-            }
-
-            void degree(float rotate) override
-            {
-                // Floor tal och sen map()
-                // rotation = map();
-            }
-
-            void off() override
-            {
-                change(false);
-            }
-
-            void on() override
-            {
-                change(true);
-            }
-
-            void percent(float rotate) override {
-                degree((180.0 * rotate) - 90.0);
-            }
-
-            void update()
-            {
-                if (active)
+            public:
+                Servo(T pin)
+                    : pin(pin), timer(DelayAsync(0))
                 {
-                    // Mät om tillräckligt med tid har passerat, i så fall stäng av eller på
+                    this->pin.initiate(pin::Mode::OutputDigital);
+                    off();
                 }
-                else
+                ~Servo()
                 {
-                    pin.digitalwrite(false);
+                    off();
                 }
-            }
 
-          protected:
-            T pin;
-            int time = 0;
+                void degree(const double rotate) override
+                {
+                    rotation = map<double>(rotate, -90.0, 90.0, 0.001, 0.002);
+                    // Floor tal och sen map()
+                    // rotation = map();
+                }
+
+                void off() override
+                {
+                    change(false);
+                }
+
+                void on() override
+                {
+                    change(true);
+                }
+
+                void percent(double rotate) override
+                {
+                    degree((180.0 * rotate) - 90.0);
+                }
+
+                void update()
+                {
+                    if (active)
+                    {
+
+                    }
+                }
+
+            protected:
+                T pin;
+                DelayAsync timer;
+
+                void change(const bool state)
+                {
+                    active = state;
+                }
         };
     }
 }
