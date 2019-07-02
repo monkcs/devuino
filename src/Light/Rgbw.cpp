25f42e2a3f3afd337eec4013a693c1528a247df5
Monkcs studios <ape@monkcs.se>
2019-07-02 14:43:35 +0200

    Move .cpp file into .h because if used in template it is needed

--- src/Light/Rgbw.cpp
+++ src/Light/Rgbw.cpp
@@ -3,15 +3,10 @@
 namespace devuino
 {
     void Rgbw::brightness(const int value)
-    {
-        bright = value;
-        colour(r, g, b, w);
-    }
+
 
     void Rgbw::colour(const uint32_t hexadecimal) const
-    {
-        colour(static_cast<uint8_t>(hexadecimal >> 24), static_cast<uint8_t>(hexadecimal >> 16), static_cast<uint8_t>(hexadecimal >> 8), static_cast<uint8_t>(hexadecimal));
-    }
+
 
     void Rgbw::colour(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t w)
     {
--- src/Light/Rgbw.h
+++ src/Light/Rgbw.h
@@ -9,9 +9,27 @@ namespace devuino
     class Rgbw : public Rgb
     {
       public:
-        void brightness(const int value) override;
-        void colour(const uint32_t hexadecimal) const;
-        void colour(const uint8_t red, const uint8_t green, const uint8_t blue, const uint8_t white);
+        void brightness(const int value) override
+        {
+            bright = value;
+            colour(r, g, b, w);
+        };
+
+        void colour(const uint32_t hexadecimal) const
+        {
+            colour(static_cast<uint8_t>(hexadecimal >> 24),
+                   static_cast<uint8_t>(hexadecimal >> 16),
+                   static_cast<uint8_t>(hexadecimal >> 8),
+                   static_cast<uint8_t>(hexadecimal));
+        };
+
+        void colour(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t w)
+        {
+            Rgb::colour(r, g, b);
+            this->w = w;
+            white(w);
+        };
+
         virtual void white(const uint8_t value) = 0;
 
       protected:
