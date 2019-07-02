971819719f0d86c343bee121998eb46979eecb28
Monkcs studios <ape@monkcs.se>
2019-07-02 14:23:04 +0200

    Move .cpp file into .h because if used in template it is needed

--- src/Light/Rgb.h
+++ src/Light/Rgb.h
@@ -9,10 +9,32 @@ namespace devuino
     class Rgb : public Light
     {
       public:
-        Rgb();
-        void brightness(const int value) override;
-        void colour(const uint32_t hexadecimal) const;
-        void colour(const uint8_t r, const uint8_t g, const uint8_t b);
+        Rgb() : Light(8)
+        { };
+
+        void brightness(const int value) override
+        {
+            bright = value;
+            colour(r, g, b);
+        };
+
+        void colour(const uint32_t hexadecimal) const
+        {
+            colour(static_cast<uint8_t>(hexadecimal >> 16),
+                   static_cast<uint8_t>(hexadecimal >> 8),
+                   static_cast<uint8_t>(hexadecimal));
+        };
+
+        void colour(const uint8_t r, const uint8_t g, const uint8_t b)
+        {
+            this->r = r;
+            this->g = g;
+            this->b = b;
+            red(r);
+            green(g);
+            blue(b);
+        };
+
         virtual void red(const uint8_t value) = 0;
         virtual void green(const uint8_t value) = 0;
         virtual void blue(const uint8_t value) = 0;
