383f8f37e5bf913db8a60e761edde1425011bee0
monkcs <charlie.habolin@monkcs.se>
2020-11-22 00:54:59 +0100

    Initial Neopixel implemented

---
+++ src/Device/Neopixel/WS2812.h
@@ -0,0 +1,40 @@
+#ifndef WS2812_H
+#define WS2812_H
+
+namespace devuino
+{
+	namespace device
+	{
+		struct WS2812 final
+		{
+			struct one final
+			{
+				static const unsigned int high = 900;
+				static const unsigned int low = 600;
+			};
+
+			struct zero final
+			{
+				static const unsigned int high = 400;
+				static const unsigned int low = 900;
+			};
+		};
+
+		struct WS2812B final
+		{
+			struct one final
+			{
+				static const unsigned int high = 800;
+				static const unsigned int low = 450;
+			};
+
+			struct zero final
+			{
+				static const unsigned int high = 400;
+				static const unsigned int low = 850;
+			};
+		};
+	}
+}
+
+#endif
