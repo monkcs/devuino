750b86cd48bc1b9506e078c98847518e910c3263
monkcs <charlie.habolin@monkcs.se>
2020-11-23 23:57:43 +0100

    Break out initializer assignment calculation

--- src/Resolution/Resolution.h
+++ src/Resolution/Resolution.h
@@ -11,10 +11,16 @@ namespace devuino
 		/* Set the bits and compute the resolution with a cap on 15 bits,
 		   (because currently we are using a uint16_t int to return it. */
 
-		explicit constexpr Resolution(const uint8_t bit) : bits((bit < 15) ? bit : 15), maximum((1 << ((bit < 15) ? bit : 15)) - 1) {};
+		explicit constexpr Resolution(const uint8_t bit) : bits((bit < 15) ? bit : 15), maximum(calculate(bit)) {};
 
 		uint8_t bits;
 		uint16_t maximum;
+
+	  private:
+		constexpr uint16_t calculate(const uint8_t bit) const
+		{
+			return (1 << ((bit < 15) ? bit : 15)) - 1;
+		}
 	};
 }
 
