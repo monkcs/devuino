1591889211bb4aad156e2411aab06d48a55ec305
monkcs <charlie.habolin@monkcs.se>
2021-01-24 00:49:27 +0100

    Update to use brace initilization

---
+++ src/Display/Vector3D.hpp
@@ -0,0 +1,24 @@
+#ifndef VECTOR3D_HPP
+#define VECTOR3D_HPP
+
+#include "Vector2D.hpp"
+
+namespace devuino
+{
+	template<typename T>
+	class Vector3D : public Vector2D
+	{
+	  public:
+		constexpr Vector3D(const T x, const T y, const T z) : Vector2D {x, y}, z {z} {};
+
+		T z;
+
+		constexpr T area() = delete;
+		constexpr T volume() const
+		{
+			return x * y * z;
+		}
+	};
+}
+
+#endif
