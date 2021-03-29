54938ab50f2f96fa046955c84a52d289d37e313d
monkcs <charlie.habolin@monkcs.se>
2021-03-29 23:35:46 +0200

    Compress namespace

--- src/Tools/Iterator.hpp
+++ src/Tools/Iterator.hpp
@@ -2,29 +2,17 @@
 
 /* https://www.nextptr.com/tutorial/ta1208652092/how-cplusplus-rangebased-for-loop-works */
 
-namespace devuino
+namespace devuino::tools
 {
-	namespace tools
+	template<typename T>
+	struct Iterator final
 	{
-		template<typename T>
-		struct Iterator final
-		{
-			T* pointer;
+		T* pointer;
 
-			constexpr T& operator*()
-			{
-				return *pointer;
-			};
+		constexpr T& operator*() { return *pointer; };
 
-			constexpr bool operator!=(const Iterator& rhs) const
-			{
-				return pointer != rhs.pointer;
-			};
+		constexpr bool operator!=(const Iterator& rhs) const { return pointer != rhs.pointer; };
 
-			void operator++()
-			{
-				++pointer;
-			};
-		};
-	}
+		void operator++() { ++pointer; };
+	};
 }
