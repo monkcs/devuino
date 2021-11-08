82182bcf0c2f9a1ee6c1f065687b236e34054f4e
Charlie Habolin <charlie.habolin@citroner.blog>
2021-11-08 22:32:02 +0100

    Add Button example

---
+++ examples/Button/Button.ino
@@ -0,0 +1,20 @@
+#include "../../src/Devuino.hpp"
+
+/* Instantiate a button connected between pin 3 and ground.
+   By using the internal pull-up resistor no external resistor is needed */
+Button<Onboard> button {Onboard {3}, Resistor::PullUp};
+
+/* Instantiate the built-in led on the arduino uno board */
+Light<Onboard> led {Onboard {13}};
+
+void setup()
+{
+	/* Read the value from the button to a bool, if it pressed or not */
+	const bool reading = button;
+}
+
+void loop()
+{
+	/* Set the led to the value of the button */
+	led = button;
+}
