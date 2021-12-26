c90416ad3b81f70577d7c15634a66eec2be044a5
Charlie Habolin <charlie.habolin@citroner.blog>
2021-12-26 22:54:06 +0100

    Move parser into constructor

--- src/Display/CharacterDisplay/SegmentDisplay/SevenSegmentString.hpp
+++ src/Display/CharacterDisplay/SegmentDisplay/SevenSegmentString.hpp
@@ -14,25 +14,64 @@ namespace devuino
 	  public:
 		constexpr SevenSegmentString() : devuino::tools::Array<SevenSegmentCharacter, lenght> {} { }
 
-		SevenSegmentString(const tools::Stringview& string) { parse(string); }
+		SevenSegmentString(const tools::Stringview string)
+		{
+			/*
+			   The constructor copy the characters from the stringview to SevenSegmentCharacters.
+			   It checks if a dot '.' is present and then "merges" it to the previous character
+			   in the buffer, as seven segment displays have a dedicated dot on each character.
+			*/
+
+			auto iterator_buffer = this->begin();
+			auto iterator_string = string.begin();
+
+			bool previously_merged = false;
+
+			while (iterator_buffer != this->end() && iterator_string != string.end())
+			{
+				if (*iterator_string == '.' && previously_merged == false && iterator_buffer != this->begin())
+				{
+					iterator_buffer.previous() |= '.';
+					previously_merged = true;
+				}
+				else
+				{
+					*iterator_buffer = *iterator_string;
+
+					previously_merged = false;
+					++iterator_buffer;
+				}
+				++iterator_string;
+			}
+
+			if (iterator_string != string.end())
+			{
+				if (*iterator_string == '.')
+				{
+					iterator_buffer.previous() |= '.';
+				}
+			}
+		}
 
 		constexpr explicit operator const SevenSegmentCharacter*() const { return this->buffer; }
 		constexpr explicit operator SevenSegmentCharacter*() { return this->buffer; }
 
 		SevenSegmentString& operator=(const tools::Stringview& string)
 		{
-			for (auto& character : this->buffer)
-			{
-				character = {};
-			}
-
-			parse(string);
+			this->fill({});
+			*this = SevenSegmentString {string};
 
 			return *this;
 		}
 
-		/* Number of elements */
+		/* Number of characters */
 		constexpr size_t size() const { return lenght; }
+
+		/* Bound checked index access, access outside string returns empty character */
+		SevenSegmentCharacter at(const size_t position) const
+		{
+			return position < lenght ? this->buffer[position] : SevenSegmentCharacter {};
+		}
 
 		SevenSegmentCharacter operator[](const size_t position) const { return this->buffer[position]; }
 		SevenSegmentCharacter& operator[](const size_t position) { return this->buffer[position]; }
@@ -54,41 +93,5 @@ namespace devuino
 			return true;
 		}
 		bool operator!=(const SevenSegmentString<lenght>& other) const { return !(this == other); }
-
-	  protected:
-		void parse(const tools::Stringview& string)
-		{
-			auto iterator_buffer = this->begin();
-			auto iterator_string = string.begin();
-
-			bool previous = false;
-
-			while (iterator_buffer != this->end() && iterator_string != string.end())
-			{
-				if (*iterator_string == '.' && previous == false && iterator_buffer != this->begin())
-				{
-					const char character = SevenSegmentCharacter {iterator_string.previous()} | 0x80;
-					iterator_buffer.previous() = {character, true};
-					previous = true;
-				}
-				else
-				{
-					*iterator_buffer = *iterator_string;
-
-					previous = false;
-					++iterator_buffer;
-				}
-				++iterator_string;
-			}
-
-			if (iterator_string != string.end())
-			{
-				if (*iterator_string == '.')
-				{
-					const char character = SevenSegmentCharacter {iterator_string.previous()} | 0x80;
-					iterator_buffer.previous() = {character, true};
-				}
-			}
-		}
 	};
 }
